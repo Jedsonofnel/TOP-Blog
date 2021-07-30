e5abc6747f964cf846963052b299cf3cdc6e7c79
jedn <jed@nelson.ac>
2021-07-30 16:28:10 +0200

    Initial commit

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# code for article model object
+class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+end
